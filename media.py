b953c697d6cb010da97cc262813ecd467d07a2c7
Chike Ezeh <ezeh.chike@gmail.com>
2017-07-04 15:17:30 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,14 @@
 class Movie():
-    """ This Class provides a way to store movie related information"""
+    """ This Class provides a way to store movie related information
+        Such as title, storyline, poster image url, and trailer.
+        To create and instance of the Movie class, import media into
+        your python file then create an instance by:
+        variable = media.Movie(movie_title, movie_storyline,
+        poster_image, trailer_youtube)
+    """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
 
         self.title = movie_title
         self.storyline = movie_storyline
